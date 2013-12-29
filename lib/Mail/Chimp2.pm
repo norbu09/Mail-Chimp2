b556629d43cc462b7d12e99a54897b6ca898435b
Lenz Gschwendtner <lenz@springtimesoft.com>
2013-12-30 12:42:38 +1300

    initial commit

---
+++ lib/Mail/Chimp2.pm
@@ -0,0 +1,90 @@
+package Mail::Chimp2;
+
+use 5.012;
+use Mouse;
+
+# ABSTRACT: 
+
+# VERSION
+
+use Data::Dump;
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Some::Module;
+    use Data::Dump 'dump';
+
+=head1 ATTRIBUTES
+
+=head2 attr
+
+=cut
+
+has 'attr' => (
+    is => 'rw',
+);
+
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2 method
+
+=cut
+
+sub method {
+    my ($self) = @_;
+}
+
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests on GitHub's issue tracker L<https://github.com/norbu09/Mail::Chimp2/issues>.
+Pull requests welcome.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Mail::Chimp2
+
+
+You can also look for information at:
+
+=over 4
+
+=item * GitHub repository
+
+L<https://github.com/norbu09/Mail::Chimp2>
+
+=item * MetaCPAN
+
+L<https://metacpan.org/module/Mail::Chimp2>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Mail::Chimp2>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Mail::Chimp2>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+=over 4
+
+=back
+
+=cut
+
+__PACKAGE__->meta->make_immutable();  # End of Mail::Chimp2
