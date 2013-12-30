23e4952526c167598569a5ca66e092a3ed9afd30
Lenz Gschwendtner <lenz@springtimesoft.com>
2013-12-30 14:51:07 +1300

    satisfy dzil

--- bin/import.pl
+++ bin/import.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/env perl
+#
+# PODNAME: import.pl
+# VERSION:
+# ABSTRACT: simple CSV importer
 
 use 5.010;
 use lib 'lib';
@@ -6,12 +10,17 @@ use Mail::Chimp2;
 use Text::CSV;
 use Data::Dump qw/dump/;
 
+#### CONFIG SECTION ###
 my $api_key = 'API KEY';
 my $list_id = 'LIST ID';
+my $sep_char = ';';
+#######################
+
+
 my $file     = shift;
 die "Usage: $0 [CSV File]\n" unless $file;
 
-my $csv = Text::CSV->new ( { binary => 1, sep_char => ';' } ) 
+my $csv = Text::CSV->new ( { binary => 1, sep_char => $sep_char } ) 
     or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
 open my $fh, "<:encoding(utf8)", $file or die "Could not open $file: $!";
