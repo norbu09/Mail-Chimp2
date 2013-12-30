f6f3c7b265d7afa904b6b761ee49019fe03a9edc
Lenz Gschwendtner <lenz@springtimesoft.com>
2013-12-30 14:47:53 +1300

    added a basic CSV importer

---
+++ bin/import.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use lib 'lib';
+use Mail::Chimp2;
+use Text::CSV;
+use Data::Dump qw/dump/;
+
+my $api_key = 'API KEY';
+my $list_id = 'LIST ID';
+my $file     = shift;
+die "Usage: $0 [CSV File]\n" unless $file;
+
+my $csv = Text::CSV->new ( { binary => 1, sep_char => ';' } ) 
+    or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
+open my $fh, "<:encoding(utf8)", $file or die "Could not open $file: $!";
+my $batch;
+while ( my $row = $csv->getline( $fh ) ) {
+    push(@{$batch}, {
+            email      => { email => $row->[0] },
+            merge_vars => {
+                FNAME   => $row->[1],
+                LNAME   => $row->[2],
+                DOMAINS => $row->[3],
+        }});
+}
+
+my $chimp = Mail::Chimp2->new(api_key => $api_key);
+my $ok = $chimp->lists_batch_subscribe(
+    id              => $list_id,
+    double_optin    => 'false',
+    update_existing => 'true',
+    batch           => $batch);
+dump $ok;
