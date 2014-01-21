5e96bc384ca2c00deb292d7f51b47cfca03cc6f5
Lenz Gschwendtner <lenz@springtimesoft.com>
2014-01-21 16:31:27 +1300

    parameter handling for importer

--- bin/import.pl
+++ bin/import.pl
@@ -10,37 +10,53 @@
 
 use strict;
 use 5.010;
-use lib 'lib';
+#use lib 'lib';
 use Mail::Chimp2;
 use Text::CSV;
+use Getopt::Long;
 use Data::Dump qw/dump/;
 
-#### CONFIG SECTION ###
-my $api_key = 'API KEY';
-my $list_id = 'LIST ID';
+my $api_key;
+my $list_id;
 my $sep_char = ';';
-#######################
+my $file;
+my $debug = 0;
 
+GetOptions(
+    "debug|d"    => \$debug,
+    "apikey|a=s" => \$api_key,
+    "listid|l=s"   => \$list_id,
+    "file|f=s"     => \$file,
+) or die;
 
-my $file     = shift;
 die "Usage: $0 [CSV File]\n" unless $file;
 
-my $csv = Text::CSV->new ( { binary => 1, sep_char => $sep_char } ) 
-    or die "Cannot use CSV: ".Text::CSV->error_diag ();
+my $csv = Text::CSV->new({ binary => 1, sep_char => $sep_char })
+    or die "Cannot use CSV: " . Text::CSV->error_diag();
 
 open my $fh, "<:encoding(utf8)", $file or die "Could not open $file: $!";
+say "reading CSV: $file" if $debug;
 my $batch;
-while ( my $row = $csv->getline( $fh ) ) {
-    push(@{$batch}, {
+# format:
+# *|EMAIL|* *|FNAME|* *|LNAME|* *|DOMAINS|*
+#
+while (my $row = $csv->getline($fh)) {
+
+    next unless $row->[0];
+    push(
+        @{$batch}, {
             email      => { email => $row->[0] },
             merge_vars => {
                 FNAME   => $row->[1],
                 LNAME   => $row->[2],
                 DOMAINS => $row->[3],
-        }});
+            } });
 }
 
+dump $batch if $debug;
+
 my $chimp = Mail::Chimp2->new(api_key => $api_key);
+
 my $ok = $chimp->lists_batch_subscribe(
     id              => $list_id,
     double_optin    => 'false',
